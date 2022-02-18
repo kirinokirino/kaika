212564a22525313f1dedf8ea5993039259c46a80
kirinokirino <kirinokirino@lunariko.com>
2022-02-18 22:17:00 +0300

    Basic window.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+
+use macroquad::prelude::*;
+
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "game".to_owned(),
+        fullscreen: false,
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(window_conf)]
+async fn main() {
+    loop {
+        clear_background(WHITE);
+        next_frame().await
+    }
 }
