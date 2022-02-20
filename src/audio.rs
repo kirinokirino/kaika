59cd73579d28e760014b135073598a378f5790a2
kirinokirino <kirinokirino@lunariko.com>
2022-02-21 01:38:20 +0300

    Store samples in audio hashmap, load from paths file.

---
+++ src/audio.rs
@@ -0,0 +1,51 @@
+use std::collections::HashMap;
+
+use macroquad::audio::Sound;
+use macroquad::ui;
+
+const SOUND_PATHS: &str = include_str!("sounds.txt");
+
+pub struct Audio {
+    samples: HashMap<&'static str, Sample>,
+}
+
+impl Audio {
+    pub async fn new() -> Self {
+        let paths = SOUND_PATHS.split('\n');
+        let mut samples = HashMap::with_capacity(5);
+        for path in paths {
+            let name = path
+                .split('/')
+                .last()
+                .unwrap()
+                .split('.')
+                .take(1)
+                .last()
+                .unwrap();
+            let sample = Sample::new(path).await;
+            samples.insert(name, sample);
+        }
+        Self { samples }
+    }
+
+    pub fn debug(&self) {
+        for (name, sample) in &self.samples {
+            if ui::root_ui().button(None, *name) {
+                macroquad::audio::play_sound_once(sample.sound);
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Sample {
+    path: &'static str,
+    sound: Sound,
+}
+
+impl Sample {
+    pub async fn new(path: &'static str) -> Self {
+        let sound = macroquad::audio::load_sound(path).await.unwrap();
+        Self { path, sound }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use macroquad::prelude::*;
-use macroquad::{audio, ui};
 
+mod audio;
 mod world;
 
 fn window_conf() -> Conf {
@@ -15,39 +15,12 @@ fn window_conf() -> Conf {
 async fn main() {
     set_pc_assets_folder("assets");
 
-    let sound1 = audio::load_sound("samples/unfa-ui-open.ogg").await.unwrap();
-    let sound2 = audio::load_sound("samples/unfa-ui-close.ogg")
-        .await
-        .unwrap();
-    let sound3 = audio::load_sound("samples/unfa-ui-hide.ogg").await.unwrap();
-    let sound4 = audio::load_sound("samples/unfa-ui-select.ogg")
-        .await
-        .unwrap();
-    let sound5 = audio::load_sound("samples/unfa-radar.ogg").await.unwrap();
+    let audio = audio::Audio::new().await;
 
     loop {
         clear_background(GRAY);
 
-        if ui::root_ui().button(None, "Play sound 1") {
-            warn!("play 1!");
-            audio::play_sound_once(sound1);
-        }
-        if ui::root_ui().button(None, "Play sound 2") {
-            warn!("play 2!");
-            audio::play_sound_once(sound2);
-        }
-        if ui::root_ui().button(None, "Play sound 3") {
-            warn!("play 3!");
-            audio::play_sound_once(sound3);
-        }
-        if ui::root_ui().button(None, "Play sound 4") {
-            warn!("play 4!");
-            audio::play_sound_once(sound4);
-        }
-        if ui::root_ui().button(None, "Play sound 5") {
-            warn!("play 5!");
-            audio::play_sound_once(sound5);
-        }
+        audio.debug();
 
         next_frame().await
     }
